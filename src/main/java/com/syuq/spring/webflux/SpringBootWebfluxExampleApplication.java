4f642fcbbfc846362f40c92e76b3b88f4919f74a
Syuq <tatd.wine@gmail.com>
2023-12-15 01:17:12 +0700

    Second commit

--- src/main/java/com/syuq/spring/webflux/SpringBootWebfluxExampleApplication.java
+++ src/main/java/com/syuq/spring/webflux/SpringBootWebfluxExampleApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
-//import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 import org.springframework.web.reactive.config.EnableWebFlux;
@@ -12,7 +11,6 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 import io.r2dbc.spi.ConnectionFactory;
 
 @EnableWebFlux
-//@EnableR2dbcRepositories
 @SpringBootApplication
 public class SpringBootWebfluxExampleApplication {
 
